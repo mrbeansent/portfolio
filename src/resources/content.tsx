f561bb55ceff630bc81a987b0a7474fce7579abd
AmielVincent <vincentcanas@gmail.com>
2026-02-28 00:37:23 +0800

    Update Name

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -102,7 +102,7 @@ const about: About = {
     title: "Introduction",
     description: (
       <>
-        Vincent is a PH-based design engineer with a passion for transforming complex challenges
+        Amiel Vincent is a PH-based design engineer with a passion for transforming complex challenges
         into simple, elegant design solutions. Her work spans digital interfaces, interactive
         experiences, and the convergence of design and technology.
       </>
